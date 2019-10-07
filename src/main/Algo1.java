a29fa438dc87a96f9e1b0ca51d3d4d29f5bef5b6
Szuhanyi Gyorgy <gyorgy.szuhanyi@tba.group>
2019-10-07 15:32:40 +0300

    a204

--- src/main/Algo1.java
+++ src/main/Algo1.java
@@ -33,8 +33,8 @@ public class Algo1 {
                 }
             }
         }
-        System.out.println("N.o. numbers which divident by 7 modulo equals 5 "+counterSeven);
-        System.out.println("N.o numcounterThirteen");
+        System.out.println("N.o. numbers which divident by 7 modulo and equals to 5 = "+counterSeven);
+        System.out.println("N.o numcounterThirteen " + counterThirteen);
     }
 
 
