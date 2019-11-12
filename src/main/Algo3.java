ce64337a53aa83046df8362b14b6dee2e78d3ee3
Szuhanyi Gyorgy <szuhanyi.gyorgy@gmail.com>
2019-11-12 20:54:47 +0200

    a201

--- src/main/Algo3.java
+++ src/main/Algo3.java
@@ -1,10 +1,26 @@
 package main;
 
+import java.util.Arrays;
+
 public class Algo3 {
+
     public static void main (String [] args) {
+        Algo3 t = new Algo3();
+        t.a301();
 
     }
 
 
 
+    private void a302() {
+
+    }
+
+    private void a301() {
+        // n student's grades
+        // print the avg
+        double [] n = new double[]{1,2,3,4,5,6,7,8,9};
+        double s = Arrays.stream(n).sum();
+        System.out.println(s);
+    }
 }
